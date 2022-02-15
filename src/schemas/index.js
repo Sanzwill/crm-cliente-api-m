37e0dd9116581b8c21371cd7dfd7f0f34ad89e45
Sanzwill <hgenca2266931@gmail.com>
2022-02-15 10:01:17 -0800

    actualizacion de datos

---
+++ src/schemas/index.js
@@ -0,0 +1,43 @@
+//const { response } = require("express")
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors");
+
+const config = require("./config")
+
+const DBConnection = require("./database/DBConnection")
+const productRoutes = require("./routes/product.routes")
+const customerRoutes = require("./routes/customer.routes")
+
+// Camel case . - se entiende mejor el nombre de la variable, primera palabra con mayuscula y la siguiente empieza con mayuscula
+async function startExpressServer(params){
+    const app = express()
+    
+    const baseURl = "/api/v1"
+
+    app.use(morgan("dev"))
+
+    app.use(cors())
+
+    app.use(express.json())
+
+    
+
+    app.use(`${baseURl}/product`, productRoutes)
+
+    app.use(`${baseURl}/customer`, customerRoutes)
+
+    app.get('/', (request, response)=>{
+   
+        response.json({ message: 'Hola desde el servidor Express.js'})
+
+    });
+
+    await DBConnection();
+
+    
+    app.listen(config.port, ()=>{
+        console.log(`Server listo en http://localhost:${config.port}`);
+    });
+}
+startExpressServer();
